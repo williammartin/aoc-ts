2a1ca95d481afb715507477dde25d45365efa5f8
Will Martin <will@storyscript.io>
2020-12-30 14:19:45 +0100

    Add solution for Day 5 Part 2

--- src/day05/part2.ts
+++ src/day05/part2.ts
@@ -1,5 +1,14 @@
-import { parseInput } from '../util';
+import { parseInput } from "../util";
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: false } });
 
-// TODO: Complete Part 2
+const binaryInput = input.map((line) =>
+  line.replace(/F|L/g, "0").replace(/B|R/g, "1")
+);
+
+const decimalInput = binaryInput.map((line) => parseInt(line, 2));
+const orderedInput = decimalInput.sort((a: number, b: number) => a - b);
+
+export default orderedInput.find(
+  (x: number) => !orderedInput.includes(x + 1)
+)! + 1;
